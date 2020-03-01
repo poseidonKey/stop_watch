ee1af162583afd8b081e50ff4af69157b578b118
jeoung seoungyoung <jsy1030@chollian.net>
2020-03-01 17:24:17 +0900

    기본골격 완성

--- lib/main.dart
+++ lib/main.dart
@@ -6,30 +6,100 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp( 
-      title: 'Flutter Clip Demo',
+      title: 'StopWatch',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ), 
-      home: MyHomePage(),
+      home: StopWatchPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  final String myTitle = "Flutter Demo Test!";
+
+class StopWatchPage extends StatefulWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _StopWatchPageState createState() => _StopWatchPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _StopWatchPageState extends State<StopWatchPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.myTitle),
+        title: Text('StopWatch'),
+        centerTitle: true,
       ),
-      body: Container(),
+      body: _buildBody(),
+      bottomNavigationBar: BottomAppBar(
+        child: Container(
+          height: 50.0,
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => setState(() {
+          _clickButton();
+        }),
+        child: Icon(Icons.pause),//_isRunning ? Icon(Icons.pause) : Icon(Icons.play_arrow),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
     );
+  }
+  Widget _buildBody() {
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.only(top: 30),
+        child: Stack(
+          children: <Widget>[
+            Column(
+              children: <Widget>[
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: <Widget>[
+                    Text(
+                      'sec',
+                      style: TextStyle(fontSize: 50.0),
+                    ),
+                    Text('hundredth'),
+                  ],
+                ),
+                Container(
+                  width: 100,
+                  height: 200,
+                  child: ListView(
+                    children:["aa","bb","cc"].map((time) => Text(time)).toList(),
+                  ),
+                ),
+              ],
+            ),
+            Positioned(
+              left: 10,
+              bottom: 10,
+              child: FloatingActionButton(
+                backgroundColor: Colors.deepOrange,
+                onPressed: () {}, // _reset,
+                child: Icon(Icons.rotate_left),
+              ),
+            ),
+            Positioned(
+              right: 10,
+              bottom: 10,
+              child: RaisedButton(
+                onPressed: () {
+                  setState(() {
+                    // _recordLapTime('$sec.$hundredth');
+                  });
+                },
+                child: Text('랩타임'),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+  void _clickButton() {
+
   }
 }
